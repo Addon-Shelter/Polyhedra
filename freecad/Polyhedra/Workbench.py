83e34851b9f18b2ce8943452003342b9761d9b09
PhoneDroid <73050054+PhoneDroid@users.noreply.github.com>
2025-09-15 17:21:06 -0400

    Workbench cleanup

--- freecad/Polyhedra/Workbench.py
+++ freecad/Polyhedra/Workbench.py
@@ -1,40 +1,54 @@
 
-import FreeCAD
 
 from .Utils.Resources import icon
 from .Shapes import registerCommands
 
+from FreeCAD import Gui , Qt
 
-QT_TRANSLATE_NOOP = FreeCAD.Qt.QT_TRANSLATE_NOOP
+
+translatable = Qt.QT_TRANSLATE_NOOP
+translate = Qt.translate
 
 
-class PolyhedraWorkbench(FreeCAD.Gui.Workbench):
-    translate = FreeCAD.Qt.translate
+class PolyhedraWorkbench ( Gui.Workbench ):
 
-    MenuText = translate("Workbench", "Pyramids-and-Polyhedrons")
-    ToolTip = translate(
-        "Workbench", "A workbench for generating pyramids, polyhedrons and geodesic spheres"
-    )
+    MenuText = translate('Workbench','Pyramids-and-Polyhedrons')
+    ToolTip = translate('Workbench','A workbench for generating pyramids, polyhedrons and geodesic spheres')
 
-    def __init__(self):
-
+    def __init__ ( self ):
         self.__class__.Icon = icon('Workbench')
 
+
     def Initialize(self):
-        """This function is executed when FreeCAD starts"""
 
         registerCommands()
 
-        self.list = ["Pyramid","Tetrahedron","Hexahedron","Octahedron","Dodecahedron","Icosahedron","Icosahedron_truncated",
-                     "Geodesic_sphere","RegularSolid"] # A list of command names created in the line above
-        #self.appendMenu(["An existing Menu","My submenu"],self.list) # appends a submenu to an existing menu
-        QT_TRANSLATE_NOOP = FreeCAD.Qt.QT_TRANSLATE_NOOP
-        self.appendToolbar(
-            QT_TRANSLATE_NOOP("Workbench", "Pyramids-and-Polyhedrons"), self.list
-        )  # creates a new toolbar with your commands
-        self.appendMenu(
-            QT_TRANSLATE_NOOP("Workbench", "Pyramids-and-Polyhedrons"), self.list
-        )  # creates a new menu
+        # Commands
+
+        self.list = [
+            'Pyramid' ,
+            'Tetrahedron' ,
+            'Hexahedron' ,
+            'Octahedron' ,
+            'Dodecahedron' ,
+            'Icosahedron' ,
+            'Icosahedron_truncated' ,
+            'Geodesic_sphere' ,
+            'RegularSolid'
+        ]
+
+        title = translatable('Workbench','Pyramids-and-Polyhedrons')
+
+        self.appendToolbar(title,self.list)
+        self.appendMenu(title,self.list)
+
+
+    def ContextMenu ( self , recipient ):
+
+        title = translatable('Workbench','Pyramids-and-Polyhedrons')
+
+        self.appendContextMenu(title,self.list)
+
 
     def GetClassName ( self ):
         return 'Gui::PythonWorkbench'
@@ -44,11 +58,3 @@ class PolyhedraWorkbench(FreeCAD.Gui.Workbench):
 
     def Activated ( self ):
         pass
-
-    def ContextMenu ( self , recipient ):
-
-        # "recipient" will be either "view" or "tree"
-        self.appendContextMenu(
-            QT_TRANSLATE_NOOP("Workbench", "Pyramids-and-Polyhedrons"), self.list
-        )  # add commands to the context menu
-
